1c64a4be1fa3c448332e31c7d37fee648298d39f
lu7766lu7766 <lu7766lu7766@gmail.com>
2016-10-13 18:46:12 +0800

    vue-route練習到一半

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import Vue from 'vue';
 import VueResource from 'vue-resource';
 import HeaderApp from './component/header';
 
+import VueRouter from 'vue-route';
+
 import Bus from './bus';
 
 
@@ -12,33 +14,54 @@ $(document).ready(function () {
 	//最終會是index執行，所以預設目錄會是跟目錄，要注意
 	//$("#header-jq").load("layout/header.html");
 })
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+
+
 Vue.config.degub = false;
 window.onload = function () {
-	Vue.use(VueResource);
-	var vm = new Vue({
+	Vue.use(VueRouter);
+	
+	const app = new Vue({
 		el: '#container',
-		data: { a: "456" },
-		components:{ 
-			HeaderApp
-		},
-		methods: {
-			test: function () {
-				this.a = "898";
-				this.$http.post("app.55104.com.tw:8080/library/android_connect_db.php",{
-					table:"getNews4Cell"
-				}).then(
-					data=>{
-						console.log(data);
-					},error=>{
-						console.log(error);
-				});
+		routes: {
+			'/home': {
+				componentId: 'header-app',
+				isDefault: true
+			},
+			options: {
+				hashbang: true
 			}
-		},
-		created: function () {
-			Bus.$on('chg', function (child) {
-				vm.a = "888";
-				console.log(child)
-			})
 		}
-	});
+	})
+	// Vue.use(VueResource);
+	// var vm = new Vue({
+	// 	el: '#container',
+	// 	data: { a: "456" },
+	// 	components:{ 
+	// 		HeaderApp
+	// 	},
+	// 	router,
+	// 	methods: {
+	// 		test: function () {
+	// 			this.a = "898";
+	// 			this.$http.post("app.55104.com.tw:8080/library/android_connect_db.php",{
+	// 				table:"getNews4Cell"
+	// 			}).then(
+	// 				data=>{
+	// 					console.log(data);
+	// 				},error=>{
+	// 					console.log(error);
+	// 			});
+	// 		}
+	// 	},
+	// 	created: function () {
+	// 		Bus.$on('chg', function (child) {
+	// 			vm.a = "888";
+	// 			console.log(child)
+	// 		})
+	// 	}
+	// });
 }
